17681c71d0ced1884af498158bcf3a824868e2e1
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-11-21 16:29:13 -0700

    Display the user's name

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  protected authService: AuthService = inject(AuthService);
 }
