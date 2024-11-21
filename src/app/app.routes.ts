f2c1d4c5f9c65efbe3c7714f043cdea7597bf6d0
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-11-21 15:24:47 -0700

    Add login or sign up to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { LoginOrSignUpComponent } from './components/login-or-sign-up/login-or-sign-up.component';
 
 export const routes: Routes = [
   {
     path: 'landing-page',
     component: LandingPageComponent,
+  },
+  {
+    path: 'login-or-sign-up',
+    component: LoginOrSignUpComponent,
   },
   {
     path: '',
