323666f6c370c673a87708bf27644e6aec00c697
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-11-21 16:01:38 -0700

    Begin adding dropdown styles

--- src/app/components/ui-components/dropdown/dropdown.component.ts
+++ src/app/components/ui-components/dropdown/dropdown.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  input,
+  InputSignal,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dropdown.component.html',
-  styleUrl: './dropdown.component.scss'
+  styleUrl: './dropdown.component.scss',
 })
 export class DropdownComponent {
+  public items: InputSignal<string[]> = input.required();
+  @Output() selected = new EventEmitter<string>();
+  public isOpen = false;
+  public selectedItem: string | null = null;
 
+  toggleDropdown() {
+    this.isOpen = !this.isOpen;
+  }
+
+  selectItem(item: string) {
+    this.selectedItem = item;
+    this.isOpen = false;
+    this.selected.emit(item);
+  }
 }
