14047af9f74e6c110a6c1a558a2774289ff2a261
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-11-21 15:22:54 -0700

    Make button navigate to login page

--- src/app/components/landing-page/landing-page.component.ts
+++ src/app/components/landing-page/landing-page.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
-
+  protected router: Router = inject(Router);
 }
