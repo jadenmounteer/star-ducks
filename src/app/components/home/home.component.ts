1a1a817fb771f5553af9ecf23f96e13cba272f68
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-12-02 16:38:11 -0700

    Add input for game code

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../auth/auth.service';
 import { StarDateService } from '../../services/star-date.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
@@ -14,6 +15,7 @@ export class HomeComponent {
   private starDateService: StarDateService = inject(StarDateService);
 
   protected starDate: string = this.starDateService.getCurrentStarDate();
+  protected gameCode = '';
 
   protected createNewStarship(): void {
     // FIXME show a modal asking for the starship name.
