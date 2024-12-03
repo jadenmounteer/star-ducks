cfaf97b51ac2739b812eb73bd0387fb178a1ab06
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-12-03 16:27:21 -0700

    Add function to destroy inactive game sessions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,8 +7,9 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import * as logger from 'firebase-functions/logger';
+const { onSchedule } = require('firebase-functions/v2/scheduler');
+import * as admin from 'firebase-admin';
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -17,3 +18,48 @@ import * as logger from "firebase-functions/logger";
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+/**
+ * This method is scheduled to run every day at midnight.
+ * It cleans up inactive game sessions.
+ * It only runs in prod. I can make it run in dev if I deploy to the dev environment.
+ * For now, I just delete the sessions manually in dev.
+ */
+export const cleanupInactiveSessions = onSchedule(
+  'every day 00:00',
+  async () => {
+    const firestore = admin.firestore();
+    const playerPresenceSnapshot = await firestore
+      .collection('player-presence')
+      .get();
+
+    // Get all active players
+    const activePlayers = new Set(
+      playerPresenceSnapshot.docs
+        .filter((doc) => {
+          const data = doc.data();
+          return Date.now() - data.lastSeen < 30000; // 30 seconds threshold
+        })
+        .map((doc) => doc.data().playerId)
+    );
+
+    // Get and cleanup inactive sessions
+    const sessionsSnapshot = await firestore.collection('game-sessions').get();
+    const batch = firestore.batch();
+
+    sessionsSnapshot.docs.forEach((doc) => {
+      const session = doc.data();
+      const hasActivePlayers = session.playerIds.some((id: string) =>
+        activePlayers.has(id)
+      );
+
+      if (!hasActivePlayers) {
+        batch.delete(doc.ref);
+      }
+    });
+
+    await batch.commit();
+
+    logger.log('Inactive game sessions cleaned up.');
+  }
+);
