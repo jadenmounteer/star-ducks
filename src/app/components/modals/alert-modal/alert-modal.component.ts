f854a54beacb605a0b5d58a9513b4373bb7c7b26
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2024-12-04 15:10:40 -0700

    Begin adding alert modal

---
+++ src/app/components/modals/alert-modal/alert-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component, input, InputSignal } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-modal',
+  standalone: true,
+  imports: [],
+  templateUrl: './alert-modal.component.html',
+  styleUrl: './alert-modal.component.scss',
+})
+export class AlertModalComponent {
+  public message: InputSignal<string> = input.required();
+}
