49b2d025ae4f2a024606a9de6bcf08a79750a8fa
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2025-01-30 19:30:28 -0700

    Add terminal and screen swipe functionality

--- src/app/components/screen-swiper/screen-swiper.component.ts
+++ src/app/components/screen-swiper/screen-swiper.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+export interface Terminal {
+  name: string;
+  component: any;
+}
 
 @Component({
   selector: 'app-screen-swiper',
   standalone: true,
   imports: [],
   templateUrl: './screen-swiper.component.html',
-  styleUrl: './screen-swiper.component.scss'
+  styleUrl: './screen-swiper.component.scss',
 })
 export class ScreenSwiperComponent {
+  @Input() terminals: Terminal[] = [];
+  @Input() currentIndex = 0;
+  @Output() terminalChange = new EventEmitter<number>();
 
+  onNext() {
+    if (this.currentIndex < this.terminals.length - 1) {
+      this.terminalChange.emit(this.currentIndex + 1);
+    }
+  }
+
+  onPrevious() {
+    if (this.currentIndex > 0) {
+      this.terminalChange.emit(this.currentIndex - 1);
+    }
+  }
 }
--- src/app/components/terminals/terminal/terminal.component.ts
+++ src/app/components/terminals/terminal/terminal.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
-import { ScreenSwiperComponent } from '../../screen-swiper/screen-swiper.component';
+import {
+  ScreenSwiperComponent,
+  Terminal,
+} from '../../screen-swiper/screen-swiper.component';
+import { MainViewerComponent } from '../main-viewer/main-viewer.component';
+import { FlightControlComponent } from '../flight-control/flight-control/flight-control.component';
 
 @Component({
   selector: 'app-terminal',
   standalone: true,
-  imports: [ScreenSwiperComponent],
+  imports: [ScreenSwiperComponent, MainViewerComponent, FlightControlComponent],
   templateUrl: './terminal.component.html',
   styleUrl: './terminal.component.scss',
 })
-export class TerminalComponent {}
+export class TerminalComponent {
+  terminals: Terminal[] = [
+    { name: 'Main Viewer', component: MainViewerComponent },
+    { name: 'Flight Control', component: FlightControlComponent },
+  ];
+
+  currentTerminalIndex = 0;
+
+  onTerminalChange(index: number) {
+    this.currentTerminalIndex = index;
+  }
+}
