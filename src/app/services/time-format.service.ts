dd4cc1e48301a4523d4ebb466f276a9eb49e6539
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2025-02-13 16:32:48 -0700

    Format the time to show hours as well

--- src/app/services/time-format.service.ts
+++ src/app/services/time-format.service.ts
@@ -4,9 +4,21 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class TimeFormatService {
-  public formatTime(totalSeconds: number): string {
-    const minutes = Math.floor(totalSeconds / 60);
-    const seconds = totalSeconds % 60;
-    return `${minutes}m ${seconds}s`;
+  public formatTime(seconds: number): string {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    let timeString = '';
+
+    if (hours > 0) {
+      timeString += `${hours}h `;
+    }
+    if (minutes > 0 || hours > 0) {
+      timeString += `${minutes}m `;
+    }
+    timeString += `${remainingSeconds}s`;
+
+    return timeString;
   }
 }
