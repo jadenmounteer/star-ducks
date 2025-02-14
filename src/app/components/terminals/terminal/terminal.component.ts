3accc2c3d02f2526dcd91e913c23315f6c8a766b
Jaden Mounteer <67714386+jadenmounteer@users.noreply.github.com>
2025-02-14 16:28:21 -0700

    Begin adding operations terminal

--- src/app/components/terminals/terminal/terminal.component.ts
+++ src/app/components/terminals/terminal/terminal.component.ts
@@ -5,11 +5,17 @@ import {
 } from '../../screen-swiper/screen-swiper.component';
 import { MainViewerComponent } from '../main-viewer/main-viewer.component';
 import { FlightControlComponent } from '../flight-control/flight-control/flight-control.component';
+import { OperationsComponent } from '../operations/operations.component';
 
 @Component({
   selector: 'app-terminal',
   standalone: true,
-  imports: [ScreenSwiperComponent, MainViewerComponent, FlightControlComponent],
+  imports: [
+    ScreenSwiperComponent,
+    MainViewerComponent,
+    FlightControlComponent,
+    OperationsComponent,
+  ],
   templateUrl: './terminal.component.html',
   styleUrl: './terminal.component.scss',
 })
@@ -17,6 +23,7 @@ export class TerminalComponent {
   terminals: Terminal[] = [
     { name: 'Main Viewer', component: MainViewerComponent },
     { name: 'Flight Control', component: FlightControlComponent },
+    { name: 'Operations', component: OperationsComponent },
   ];
 
   currentTerminalIndex = 0;
